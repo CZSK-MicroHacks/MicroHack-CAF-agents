be50bad643f6614459b583293ab482234cd3fd6d
Tomas Kubica <tkubica12@gmail.com>
2026-02-25 18:00:53 +0100

    rbac

--- deploy/terraform/modules/user_environment/rbac.tf
+++ deploy/terraform/modules/user_environment/rbac.tf
@@ -1,10 +1,6 @@
 locals {
-  owner_role_definition_id                          = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635"
-  blob_reader_role_definition_id                    = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.Authorization/roleDefinitions/2a2b9908-6ea1-4ae2-8e65-a410df84e7d1"
-  search_service_contributor_role_definition_id     = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.Authorization/roleDefinitions/7ca78c08-252a-4471-8644-bb5ff32d4ba0"
-  search_index_data_contributor_role_definition_id  = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.Authorization/roleDefinitions/8ebe5a00-799e-43f5-93ac-243d3dce84a7"
-  cognitive_services_openai_user_role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.Authorization/roleDefinitions/5e0bd9bd-7b93-4f28-af87-19fc36ad61bd"
-  role_assignment_ns                                = "b24988ac-6180-42a0-ab88-20f7382dd24c"
+  owner_role_definition_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.Authorization/roleDefinitions/8e3af657-a8ff-443c-a75c-2fe8c4bcb635"
+  role_assignment_ns       = "b24988ac-6180-42a0-ab88-20f7382dd24c"
 }
 
 resource "azapi_resource" "rg_owner_role_assignment" {
@@ -28,10 +24,41 @@ resource "azapi_resource" "rg_owner_role_assignment" {
   }
 }
 
+# --- Foundry account roles for the assigned user ---
+
+resource "azurerm_role_assignment" "user_foundry_ai_user" {
+  count                = var.create_role_assignment ? 1 : 0
+  scope                = azapi_resource.foundry_account.id
+  role_definition_name = "Azure AI User"
+  principal_id         = var.assigned_user_object_id
+  principal_type       = "User"
+}
+
+resource "azurerm_role_assignment" "user_foundry_ai_project_manager" {
+  count                = var.create_role_assignment ? 1 : 0
+  scope                = azapi_resource.foundry_account.id
+  role_definition_name = "Azure AI Project Manager"
+  principal_id         = var.assigned_user_object_id
+  principal_type       = "User"
+}
+
+# --- Foundry account role for the project managed identity ---
+
+resource "azurerm_role_assignment" "foundry_project_ai_user" {
+  scope                = azapi_resource.foundry_account.id
+  role_definition_name = "Azure AI User"
+  principal_id         = azapi_resource.foundry_project.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+
+  depends_on = [azapi_resource.foundry_project]
+}
+
+# --- Search and storage roles ---
+
 resource "azurerm_role_assignment" "search_storage_blob_reader" {
-  scope              = azurerm_storage_account.user.id
-  role_definition_id = local.blob_reader_role_definition_id
-  principal_id       = azurerm_search_service.user.identity[0].principal_id
+  scope                = azurerm_storage_account.user.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_search_service.user.identity[0].principal_id
 
   depends_on = [
     azurerm_search_service.user,
@@ -40,10 +67,10 @@ resource "azurerm_role_assignment" "search_storage_blob_reader" {
 }
 
 resource "azurerm_role_assignment" "foundry_project_search_service_contributor" {
-  scope              = azurerm_search_service.user.id
-  role_definition_id = local.search_service_contributor_role_definition_id
-  principal_id       = azapi_resource.foundry_project.identity[0].principal_id
-  principal_type     = "ServicePrincipal"
+  scope                = azurerm_search_service.user.id
+  role_definition_name = "Search Service Contributor"
+  principal_id         = azapi_resource.foundry_project.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
 
   depends_on = [
     azapi_resource.foundry_project,
@@ -52,10 +79,10 @@ resource "azurerm_role_assignment" "foundry_project_search_service_contributor" 
 }
 
 resource "azurerm_role_assignment" "foundry_project_search_index_data_contributor" {
-  scope              = azurerm_search_service.user.id
-  role_definition_id = local.search_index_data_contributor_role_definition_id
-  principal_id       = azapi_resource.foundry_project.identity[0].principal_id
-  principal_type     = "ServicePrincipal"
+  scope                = azurerm_search_service.user.id
+  role_definition_name = "Search Index Data Contributor"
+  principal_id         = azapi_resource.foundry_project.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
 
   depends_on = [
     azapi_resource.foundry_project,
@@ -64,10 +91,10 @@ resource "azurerm_role_assignment" "foundry_project_search_index_data_contributo
 }
 
 resource "azurerm_role_assignment" "search_foundry_openai_user" {
-  scope              = azapi_resource.foundry_account.id
-  role_definition_id = local.cognitive_services_openai_user_role_definition_id
-  principal_id       = azurerm_search_service.user.identity[0].principal_id
-  principal_type     = "ServicePrincipal"
+  scope                = azapi_resource.foundry_account.id
+  role_definition_name = "Cognitive Services OpenAI User"
+  principal_id         = azurerm_search_service.user.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
 
   depends_on = [
     azapi_resource.foundry_account,
